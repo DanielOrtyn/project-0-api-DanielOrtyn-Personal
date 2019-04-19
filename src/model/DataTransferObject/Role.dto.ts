27de9854e63b4a5f50406c36b412297826a545af
Daniel Ortyn <DanielOrtyn@comcast.net>
2019-04-19 12:54:16 -0400

    Import capitalization error possibly fixed

--- src/model/DataTransferObject/Role.dto.ts
+++ src/model/DataTransferObject/Role.dto.ts
@@ -1,6 +1,7 @@
-import { ISqlRole } from '../Database/ISQLRole.dbo';
+
 import { IRole } from '../Server/IRole';
 import { Role } from '../Server/Role';
+import { ISqlRole } from '../Database/ISqlRole.dbo';
 
 export function convertSqlRole(sqlRole: ISqlRole): IRole {
     return new Role(sqlRole.roleid, sqlRole.role);
